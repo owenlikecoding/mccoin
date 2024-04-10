966e0340ff28c16f0961fe32e5dd8c6348e17718
owen silberman <oreowen116@gmail.com>
2024-04-10 18:50:30 -0400

    njknjknkjnjk

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import Script from 'next/script';
 
 type AdsenseTypes = {
     pId: string;
 }
 
 const AdSense = ({ pId }: AdsenseTypes) => {
-    const scriptContent = `
-        <script type="text/javascript">
-            aclib.runAutoTag({
-                zoneId: 'abmvkgjtfs',
-            });
-        </script>
-    `;
+    const [scriptLoaded, setScriptLoaded] = React.useState(false);
+
+    const handleScriptLoad = () => {
+        setScriptLoaded(true);
+    };
 
     return (
-        <div dangerouslySetInnerHTML={{ __html: scriptContent }} />
+        <div>
+            <Script
+                src="//acscdn.com/script/aclib.js" // Use 'src' instead of 'url'
+                onLoad={handleScriptLoad}
+            />
+            {scriptLoaded && (
+                <div dangerouslySetInnerHTML={{
+                    __html: `
+                    <script type="text/javascript">
+                        aclib.runAutoTag({
+                            zoneId: 'abmvkgjtfs',
+                        });
+                    </script>
+                ` }} />
+            )}
+        </div>
     );
 }
 
