4dd21e74154ebb09c61929339135741472d8da14
owen silberman <oreowen116@gmail.com>
2024-04-10 18:43:08 -0400

    njnjknkjnkj

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,19 +1,21 @@
-import Script from 'next/script';
-import React from 'react'
+import React from 'react';
 
 type AdsenseTypes = {
     pId: string;
 }
 
 const AdSense = ({ pId }: AdsenseTypes) => {
+    const scriptContent = `
+        <script type="text/javascript">
+            aclib.runAutoTag({
+                zoneId: 'abmvkgjtfs',
+            });
+        </script>
+    `;
+
     return (
-        <Script
-        async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5001656241368378`}
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
-    )
+        <div dangerouslySetInnerHTML={{ __html: scriptContent }} />
+    );
 }
 
-export default AdSense
+export default AdSense;
