34da9b39f7a1ad2e9068edd498691b4c06a82ea2
owen silberman <oreowen116@gmail.com>
2024-04-10 19:09:21 -0400

    sjdlfkjlkdsjfl;kj;rklsfdjds

--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,29 +1,21 @@
-import React from 'react';
 import Script from 'next/script';
-import { useState } from 'react';
 
 const AdSense = () => {
-    const [scriptLoaded, setScriptLoaded] = React.useState(false);  
-    const handleScriptLoad = () => {
-        setScriptLoaded(true);
-    };
-    
+
+
     return (
         <div>
-            <Script
-                src="//acscdn.com/script/aclib.js" // Use 'src' instead of 'url'
-                onLoad={handleScriptLoad}
-            />
-            {scriptLoaded && (
-                <div dangerouslySetInnerHTML={{
-                    __html: `
+
+
+            <div dangerouslySetInnerHTML={{
+                __html: `
                     <script type="text/javascript">
                         aclib.runAutoTag({
                             zoneId: 'abmvkgjtfs',
                         });
                     </script>
                 ` }} />
-            )}
+ƒ
         </div>
     );
 }
