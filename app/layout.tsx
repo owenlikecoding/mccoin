8ddf6406ec700f9285c6ae662ee4eb6333b8e5de
owen silberman <oreowen116@gmail.com>
2024-04-10 22:01:15 -0400

    plz work

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AdSense from "@/components/AdSense";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <AdSense />
+        <div>
+        <Script
+                src="https://acscdn.com/script/aclib.js"
+            />
+        </div>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- components/AdSense.tsx
+++ components/AdSense.tsx
@@ -1,22 +1,38 @@
+import React from 'react';
 import Script from 'next/script';
+import Head from 'next/head';
+
+// global.d.ts
+declare global {
+    interface Window {
+       aclib: any; // You can replace 'any' with a more specific type if you know the structure of 'aclib'
+    }
+}
 
 const AdSense = () => {
-
+    const runAclib = () => {
+        if (window.aclib) {
+            window.aclib.runAutoTag({
+                zoneId: 'abmvkgjtfs',
+            });
+        } else {
+            // Optionally, you can set a timeout to retry after a delay
+            setTimeout(runAclib, 100); // Retry after 100ms
+        }
+    };
 
     return (
-        <div>
-
-
-            <div dangerouslySetInnerHTML={{
-                __html: `
-                    <script type="text/javascript">
-                        aclib.runAutoTag({
-                            zoneId: 'abmvkgjtfs',
-                        });
-                    </script>
-                ` }} />
-ƒ
-        </div>
+        <>
+            <Head>
+                <Script
+                    src="https://acscdn.com/script/aclib.js"
+                    strategy="beforeInteractive"
+                />
+            </Head>
+            <div>
+                {/* Your component content */}
+            </div>
+        </>
     );
 }
 
